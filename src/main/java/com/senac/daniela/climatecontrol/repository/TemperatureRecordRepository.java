154e67d9ee28744af6d153b3433c9a4385637409
36129382023.1N <danisegd@hotmail.com>
2024-09-19 21:39:17 -0300

    Updatee getAverageTemperatureByMonth method

--- src/main/java/com/senac/daniela/climatecontrol/repository/TemperatureRecordRepository.java
+++ src/main/java/com/senac/daniela/climatecontrol/repository/TemperatureRecordRepository.java
@@ -17,6 +17,7 @@ public interface TemperatureRecordRepository extends JpaRepository<TemperatureRe
     @Query("SELECT t FROM Temperature t WHERE t.date = CURRENT_DATE AND t.municipality.id = :id")
     Optional<TemperatureRecord> getTemperatureByMunicipality(@Param("id") int id);
 
-    @Query("SELECT AVG(t.value) FROM Temperature t WHERE t.municipality.id = :id AND FUNCTION('MONTH', t.date) = FUNCTION('MONTH', CURRENT_DATE) AND FUNCTION('YEAR', t.date) = FUNCTION('YEAR', CURRENT_DATE)")
+    @Query("""
+        SELECT AVG(dailyAvg) FROM (SELECT AVG(t.value) AS dailyAvg FROM Temperature t WHERE t.municipality.id = :id AND FUNCTION('MONTH', t.date) = FUNCTION('MONTH', CURRENT_DATE) AND FUNCTION('YEAR', t.date) = FUNCTION('YEAR', CURRENT_DATE)GROUP BY FUNCTION('DAY', t.date)) AS dailyAverages""")
     Optional<Double> getAverageTemperatureByMonth(@Param("id") int id);
 }
